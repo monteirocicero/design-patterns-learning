530c941b8f246f0b1069983f6b461f9fa28b1afc
Cicero Monteiro <cicerolmonteiro@gmail.com>
2017-04-28 19:07:36 -0300

    composite pattern - wip - model class

---
+++ src/main/java/composite/Employee.java
@@ -0,0 +1,36 @@
+package composite;
+
+import java.util.List;
+
+public class Employee {
+	
+	private String name;
+	private String dept;
+	private int salary;
+	private List<Employee> subordinates;
+	
+	public Employee(String name, String dept, int salary) {
+		super();
+		this.name = name;
+		this.dept = dept;
+		this.salary = salary;
+	}
+	
+	public void add(Employee employee) {
+		getSubordinates().add(employee);
+	}
+	
+	public void remove(Employee employee) {
+		getSubordinates().remove(employee);
+	}
+	
+	public List<Employee> getSubordinates() {
+		return subordinates;
+	}
+	
+	@Override
+	public String toString() {
+		return "[Employee :[ name : " + name + ", dept : " + dept + "salary: " + salary + " ]";
+	}
+
+}
